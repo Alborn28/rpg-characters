771ef381c7b476e19a1e450710e090288fedb420
Albin Ljungdahl <Albin.Ljungdal@se.experis.com>
2021-07-09 15:01:30 +0200

    feat: Added Character-class

---
+++ src/characters/Character.java
@@ -0,0 +1,29 @@
+package characters;
+
+import java.util.HashMap;
+
+public abstract class Character {
+    private String name;
+    private int level;
+
+    public Character(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
