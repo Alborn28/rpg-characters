c1c391e898a93e25d448d311bbe599092f35912f
Albin Ljungdahl <Albin.Ljungdal@se.experis.com>
2021-07-09 15:29:41 +0200

    feat: Added PrimaryAttributes-class

---
+++ src/attributes/PrimaryAttributes.java
@@ -0,0 +1,47 @@
+package attributes;
+
+public class PrimaryAttributes {
+    private int strength;
+    private int dexterity;
+    private int intelligence;
+    private int vitality;
+
+    public PrimaryAttributes(int strength, int dexterity, int intelligence, int vitality) {
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+        this.vitality = vitality;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+
+    public void setVitality(int vitality) {
+        this.vitality = vitality;
+    }
+}
