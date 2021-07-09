c7daa87d340a8b5a299c811b1b928562152a52e9
Albin Ljungdahl <Albin.Ljungdal@se.experis.com>
2021-07-09 16:18:39 +0200

    feat: Implemented toString method for characters

--- src/characters/Character.java
+++ src/characters/Character.java
@@ -1,60 +1,60 @@
+package characters;
+
+import attributes.PrimaryAttributes;
+import attributes.SecondaryAttributes;
+
+public abstract class Character {
+    private String name;
+    private int level;
+    private PrimaryAttributes primaryAttributes;
+    private SecondaryAttributes secondaryAttributes;
+
+    public Character(String name, int strength, int dexterity, int intelligence, int vitality) {
+        this.name = name;
+        this.level = 1;
+        this.primaryAttributes = new PrimaryAttributes(strength, dexterity, intelligence, vitality);
+
+        int health = vitality * 10;
+        int armorRating = strength + dexterity;
+        int elementalResistance = intelligence;
+        this.secondaryAttributes = new SecondaryAttributes(health, armorRating, elementalResistance);
     }
-            package characters;
 
-    import attributes.PrimaryAttributes;
-    import attributes.SecondaryAttributes;
+    public String getName() {
+        return name;
+    }
 
-    public abstract class Character {
-        private String name;
-        private int level;
-        private PrimaryAttributes primaryAttributes;
-        private SecondaryAttributes secondaryAttributes;
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public Character(String name, int strength, int dexterity, int intelligence, int vitality) {
-            this.name = name;
-            this.level = 1;
-            this.primaryAttributes = new PrimaryAttributes(strength, dexterity, intelligence, vitality);
+    public int getLevel() {
+        return level;
+    }
 
-            int health = vitality * 10;
-            int armorRating = strength + dexterity;
-            int elementalResistance = intelligence;
-            this.secondaryAttributes = new SecondaryAttributes(health, armorRating, elementalResistance);
-        }
+    protected PrimaryAttributes getPrimaryAttributes() {
+        return primaryAttributes;
+    }
 
-        public String getName() {
-            return name;
-        }
+    protected SecondaryAttributes getSecondaryAttributes() {
+        return secondaryAttributes;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public abstract void levelUp();
 
-        public int getLevel() {
-            return level;
-        }
+    public String toString() {
+        StringBuilder result = new StringBuilder();
 
-        protected PrimaryAttributes getPrimaryAttributes() {
-            return primaryAttributes;
-        }
+        result.append("Name: " + name + "\n");
+        result.append("Level: " + level + "\n");
+        result.append("Strength: " + primaryAttributes.getStrength() + "\n");
+        result.append("Dexterity: " + primaryAttributes.getDexterity() + "\n");
+        result.append("Intelligence: " + primaryAttributes.getIntelligence() + "\n");
+        result.append("Vitality: " + primaryAttributes.getVitality() + "\n");
+        result.append("Health: " + secondaryAttributes.getHealth() + "\n");
+        result.append("Armor Rating: " + secondaryAttributes.getArmorRating() + "\n");
+        result.append("Elemental Resistance: " + secondaryAttributes.getElementalResistance() + "\n");
 
-        protected SecondaryAttributes getSecondaryAttributes() {
-            return secondaryAttributes;
-        }
-
-        public abstract void levelUp();
-
-        public String toString() {
-            StringBuilder result = new StringBuilder();
-
-            result.append("Name: " + name + "\n");
-            result.append("Level: " + level + "\n");
-            result.append("Strength: " + primaryAttributes.getStrength() + "\n");
-            result.append("Dexterity: " + primaryAttributes.getDexterity() + "\n");
-            result.append("Intelligence: " + primaryAttributes.getIntelligence() + "\n");
-            result.append("Vitality: " + primaryAttributes.getVitality() + "\n");
-            result.append("Health: " + secondaryAttributes.getHealth() + "\n");
-            result.append("Armor Rating: " + secondaryAttributes.getArmorRating() + "\n");
-            result.append("Elemental Resistance: " + secondaryAttributes.getElementalResistance() + "\n");
-
-            return result.toString();
-        }
+        return result.toString();
+    }
+}
