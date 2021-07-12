2cf7dad9fb0b7b1dcb2808175d0af00be6b447be
Albin Ljungdahl <Albin.Ljungdal@se.experis.com>
2021-07-12 08:17:04 +0200

    Finished Mage-class, excluding items

--- src/characters/Character.java
+++ src/characters/Character.java
@@ -4,8 +4,71 @@ import attributes.PrimaryAttributes;
 import attributes.SecondaryAttributes;
 
 public abstract class Character {
+    private String name;
+    private int level;
+    private PrimaryAttributes primaryAttributes;
+    private SecondaryAttributes secondaryAttributes;
+
+    public Character(String name, int strength, int dexterity, int vitality, int intelligence) {
+        this.name = name;
+        level = 1;
+
+        this.primaryAttributes = new PrimaryAttributes(strength, dexterity, intelligence, vitality);
+
+        int health = vitality * 10;
+        int armorRating = strength + dexterity;
+        int elementalResistance = intelligence;
+        this.secondaryAttributes = new SecondaryAttributes(health, armorRating, elementalResistance);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public PrimaryAttributes getPrimaryAttributes() {
+        return primaryAttributes;
+    }
+
+    public SecondaryAttributes getSecondaryAttributes() {
+        return secondaryAttributes;
+    }
 
     public abstract void levelUp();
 
-    public abstract String toString();
+    private void updateSecondaryAttributes() {
+        int health = primaryAttributes.getVitality() * 10;
+        int armorRating = primaryAttributes.getStrength() + primaryAttributes.getDexterity();
+        int elementalResistance = primaryAttributes.getIntelligence();
+        this.secondaryAttributes = new SecondaryAttributes(health, armorRating, elementalResistance);
+    }
+
+    public String toString() {
+        updateSecondaryAttributes();
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("Name: " + name + "\n");
+        result.append("Level: " + level + "\n");
+        result.append("Strength: " + primaryAttributes.getStrength() + "\n");
+        result.append("Dexterity: " + primaryAttributes.getDexterity() + "\n");
+        result.append("Intelligence: " + primaryAttributes.getIntelligence() + "\n");
+        result.append("Vitality: " + primaryAttributes.getVitality() + "\n");
+        result.append("Health: " + secondaryAttributes.getHealth() + "\n");
+        result.append("Armor Rating: " + secondaryAttributes.getArmorRating() + "\n");
+        result.append("Elemental Resistance: " + secondaryAttributes.getElementalResistance() + "\n");
+
+        return result.toString();
+    }
 }
--- src/characters/Mage.java
+++ src/characters/Mage.java
@@ -4,63 +4,22 @@ import attributes.PrimaryAttributes;
 import attributes.SecondaryAttributes;
 
 public class Mage extends Character {
-    private String name;
-    private int level;
-    private PrimaryAttributes primaryAttributes;
-    private SecondaryAttributes secondaryAttributes;
-
     public Mage(String name) {
-        this.name = name;
-        this.level = 1;
-
-        int strength = 1;
-        int dexterity = 1;
-        int vitality = 5;
-        int intelligence = 8;
-        this.primaryAttributes = new PrimaryAttributes(strength, dexterity, intelligence, vitality);
-
-        int health = vitality * 10;
-        int armorRating = strength + dexterity;
-        int elementalResistance = intelligence;
-        this.secondaryAttributes = new SecondaryAttributes(health, armorRating, elementalResistance);
+        super(name, 1, 1, 5 ,8);
     }
 
     @Override
     public void levelUp() {
-        int strength = primaryAttributes.getStrength();
-        int dexterity = primaryAttributes.getDexterity();
-        int intelligence = primaryAttributes.getIntelligence();
-        int vitality = primaryAttributes.getVitality();
+        PrimaryAttributes primary = getPrimaryAttributes();
 
-        primaryAttributes.setStrength(strength += 1);
-        primaryAttributes.setDexterity(dexterity += 1);
-        primaryAttributes.setIntelligence(intelligence += 5);
-        primaryAttributes.setVitality(vitality += 3);
+        int strength = primary.getStrength();
+        int dexterity = primary.getDexterity();
+        int intelligence = primary.getIntelligence();
+        int vitality = primary.getVitality();
 
-        updateSecondaryAttributes();
-    }
-
-    private void updateSecondaryAttributes() {
-        int health = primaryAttributes.getVitality() * 10;
-        int armorRating = primaryAttributes.getStrength() + primaryAttributes.getDexterity();
-        int elementalResistance = primaryAttributes.getIntelligence();
-        this.secondaryAttributes = new SecondaryAttributes(health, armorRating, elementalResistance);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder();
-
-        result.append("Name: " + name + "\n");
-        result.append("Level: " + level + "\n");
-        result.append("Strength: " + primaryAttributes.getStrength() + "\n");
-        result.append("Dexterity: " + primaryAttributes.getDexterity() + "\n");
-        result.append("Intelligence: " + primaryAttributes.getIntelligence() + "\n");
-        result.append("Vitality: " + primaryAttributes.getVitality() + "\n");
-        result.append("Health: " + secondaryAttributes.getHealth() + "\n");
-        result.append("Armor Rating: " + secondaryAttributes.getArmorRating() + "\n");
-        result.append("Elemental Resistance: " + secondaryAttributes.getElementalResistance() + "\n");
-
-        return result.toString();
+        primary.setStrength(strength += 1);
+        primary.setDexterity(dexterity += 1);
+        primary.setIntelligence(intelligence += 5);
+        primary.setVitality(vitality += 3);
     }
 }
