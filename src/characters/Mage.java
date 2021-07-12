a2f45ca5a992c198992aa70a90604e7ff8883766
Albin Ljungdahl <Albin.Ljungdal@se.experis.com>
2021-07-12 08:40:43 +0200

    feat: Implemented the Ranger-class

--- src/characters/Mage.java
+++ src/characters/Mage.java
@@ -1,8 +1,5 @@
 package characters;
 
-import attributes.PrimaryAttributes;
-import attributes.SecondaryAttributes;
-
 public class Mage extends Character {
     public Mage(String name) {
         super(name, 1, 1, 5 ,8);
@@ -10,16 +7,6 @@ public class Mage extends Character {
 
     @Override
     public void levelUp() {
-        PrimaryAttributes primary = getPrimaryAttributes();
-
-        int strength = primary.getStrength();
-        int dexterity = primary.getDexterity();
-        int intelligence = primary.getIntelligence();
-        int vitality = primary.getVitality();
-
-        primary.setStrength(strength += 1);
-        primary.setDexterity(dexterity += 1);
-        primary.setIntelligence(intelligence += 5);
-        primary.setVitality(vitality += 3);
+        levelUpChar(1, 1, 3, 5);
     }
 }
