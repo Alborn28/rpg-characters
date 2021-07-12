80eaa1e30454b159fd325edff9a46f9ee67d58cb
Albin Ljungdahl <Albin.Ljungdal@se.experis.com>
2021-07-12 09:55:56 +0200

    feat: Implemented the Weapon-class, and a WeaponType-enumerator

--- src/Program.java
+++ src/Program.java
@@ -1,14 +1,17 @@
 import characters.*;
 import characters.Hero;
+import items.Weapon;
 
 public class Program {
     public static void main(String[] args) {
-        Hero hero = new Warrior("Max");
+        /*Hero hero = new Warrior("Max");
 
         System.out.println(hero.toString());
 
         hero.levelUp();
 
-        System.out.println(hero.toString());
+        System.out.println(hero.toString());*/
+
+
     }
 }
